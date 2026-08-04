edfceada5ab3317c59e1908b1d38eec1477fa2ba
VivekRawat29 <vivekrawat5468@gmail.com>
2026-08-04 11:07:13 +0530

    Added GitHub Actions CI/CD pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,97 @@
+resource "aws_vpc" "cloudops_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.cloudops_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-north-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.cloudops_vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.cloudops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_security_group" "cloudops_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Security Group for CloudOps EC2"
+  vpc_id      = aws_vpc.cloudops_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Application"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+resource "aws_instance" "cloudops_ec2" {
+  ami           = "ami-0cda11afd45b74b89"
+  instance_type = "t3.micro"
+
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.cloudops_sg.id]
+
+  associate_public_ip_address = true
+
+  key_name = "linux-devops"
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
+}
